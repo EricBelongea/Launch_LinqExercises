c4871191c9342d74f0bad8b9e8ea2727cd1d66c5
Eric Belongea <belongeae@infinity-software.com>
2024-04-05 11:49:50 -0600

    Works through and completes the .All() tests.

--- Launch_LinqExercises/AllTest.cs
+++ Launch_LinqExercises/AllTest.cs
@@ -10,85 +10,86 @@ namespace Launch_LinqExercises
             Assert.True(allZeros);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void NotAllZeros()
         {
             var numbers = new List<int> { 0, 0, 0, 0, 1, 0, 0 };
             var allZeros = numbers.All(n =>
-                    n == n // replace with your code .. can collapse into one line above
+                    n == 0 // replace with your code .. can collapse into one line above
             );
             Assert.False(allZeros);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void AllGone()
         {
             var words = new List<string> { "gone", "gone", "gone", "gone", "gone", "gone", "gone" };
-            var allGone = false; // replace with your code;
+            var allGone = words.All(w => w == "gone"); // replace with your code;
             Assert.True(allGone);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void NotAllGone()
         {
             var words = new List<string> { "gone", "gone", "gone", "gone", "there", "gone", "gone" };
-            var allGone = false; // replace with your code;
+            var allGone = words.All(w => w == "gone"); // replace with your code;
             Assert.False(allGone);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void AllEmpty()
         {
             var strings = new List<string> { "", "", "", "", "", "", "" };
-            var allEmpty = false; // replace with your code;
+            var allEmpty = strings.All(string.IsNullOrEmpty); // replace with your code;
+            //var allEmpty = strings.All(string.IsNullOrEmpty); // This was the original way that I did it, but I wanted a "better" way. 
             Assert.True(allEmpty);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void NotAllEmpty()
         {
             var strings = new List<string> { "", "", "", "Full", "", "", "" };
-            var allEmpty = false; // replace with your code;
+            var allEmpty = strings.All(string.IsNullOrEmpty); // replace with your code;
             Assert.False(allEmpty);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void NotAllUppercase()
         {
             var words = new List<string> { "DOUGHNUT", "CASH", "MAIN", "bOWl", "SMACK", "SAND" };
-            var allCaps = false; // replace with your code;
+            var allCaps = words.All(w => w == w.ToUpper()); // replace with your code;
             Assert.False(allCaps);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void AllLies()
         {
             var lies = new List<bool> { false, false, false, false };
-            var allLies = false; // replace with your code;
+            var allLies = lies.All(lie => lie == false); // replace with your code;
             Assert.True(allLies);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void AllMultiplesOf7()
         {
             var numbers = new List<int> { 42, 14, 35, 49, 28, 56, 21, 7 };
-            var allMultiplesOf7 = false; // replace with your code;
+            var allMultiplesOf7 = numbers.All(num => num % 7 == 0); // replace with your code;
             Assert.True(allMultiplesOf7);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void NotAll3DigitsLong()
         {
             var numbers = new List<int> { 981, 831, 509, 332, 892, 8999, 110 };
-            var all3Digits = false; // replace with your code;
+            var all3Digits = numbers.All(num => num.ToString().Length == 3); // replace with your code;
             Assert.False(all3Digits);
         }
 
-        [Fact(Skip = "Remove this when code is complete")]
+        [Fact]
         public void All4LetterWords()
         {
             var words = new List<string> { "love", "hate", "fire", "bird", "call" };
-            var all4Letters = false; // replace with your code;
+            var all4Letters = words.All(word => word.Length == 4); // replace with your code;
             Assert.True(all4Letters);
         }
     }
